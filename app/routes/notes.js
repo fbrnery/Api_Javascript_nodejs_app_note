3c83ca78d79d4e4994469c6be842f2240e90c292
Fabricio Miranda <fbrnery@gmail.com>
2022-06-11 20:46:42 -0300

    Baixando uma nota

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -12,8 +12,30 @@ router.post('/', withAuth, async (req, res) => {
          await note.save();
          res.status(200).json(note);
        } catch (error) {
-         res.status(500).json({error: 'Problem to create a new note'});
+         res.status(500).json({error: 'Problem to create a note'});
        }
    });
+
+   router.get('/:id', withAuth, async function(req, res) {
+
+    try {
+        const { id } = req.params;
+        let note = await Note.findById(id);
+        if(is_owner(req.user, note))
+        res.json(note);
+        else
+        res.status(403).json({error: 'Permission denied'});
+    }   catch (error) {
+        res.status(500).json({error: 'Problem to get a note'}).status(500)
+    }
+});
+
+const is_owner = (user, note) => {
+     if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
+       return true;
+     else
+       return false;
+   }
+
 
    module.exports = router;
