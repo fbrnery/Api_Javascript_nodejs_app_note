244bc4eef6e5f99093d1864a12b2e05d133df3b1
Fabricio Miranda <fbrnery@gmail.com>
2022-06-16 20:30:55 -0300

    Atualizando uma nota

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -37,14 +37,32 @@ router.get('/', withAuth, async(req, res) => {
     }   catch (error) {
         res.json({error: error}).status(500);
     }
-});
+})
 
-const is_owner = (user, note) => {
+router.put('/:id', withAuth, async(req, res) => {
+    const { title, body } = req.body;
+    const { id } = req.params;
+
+    try {
+        let note = await Note.findById(id);
+        if(isOwner(req.user, note)){
+            let note = await Note.findOneAndUpdate(id,
+                { $set: { title: title, body: body } },
+                { upsert: true, 'new': true }
+              );
+              res.json(note);
+        } else
+        res.status(403).json({error: 'Permission denied'});
+    }   catch (error) {
+       res.status(500).json({error: 'Problem to update a note'});
+    }
+})
+
+const isOwner = (user, note) => {
      if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
        return true;
      else
        return false;
    }
-
 
    module.exports = router;
